0b88dc4d63cc677767db80d9792e3545cb273a5e
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2023-02-16 12:23:32 +0500

    chnages

--- src/components/datepicker.jsx
+++ src/components/datepicker.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Dayjs } from 'dayjs';
 import TextField from '@mui/material/TextField';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -14,7 +13,7 @@ function Datepicker({ control, label }) {
             <Controller
                 name={label}
                 control={control}
-                defaultValue={moment(new Date()).format("YYYY-MM-DD")}
+                defaultValue={moment(new Date()).format("MM-DD-YYYY")}
                 render={({
                     field: { onChange, value },
                     fieldState: { error, invalid }
@@ -23,7 +22,7 @@ function Datepicker({ control, label }) {
                         label={label}
                         value={value}
                         onChange={(newvalue) => {
-                            onChange(moment(newvalue).format("YYYY-MM-DD"))
+                            onChange(moment(newvalue).format("MM-DD-YYYY"))
                         }}
                         renderInput={(params) => <TextField {...params} style={{ width: '300px', backgroundColor: "white" }} />}
                     />
